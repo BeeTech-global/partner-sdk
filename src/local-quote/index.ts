ea07f5e09daadf5583ac43e39e911c64819d468e
Eduardo Romanini <eduardo.c.romanini@gmail.com>
2022-10-06 15:37:34 -0300

    fix(tax-calculator): remove injection from QuoteCalculator  #id 46039

--- src/local-quote/index.ts
+++ src/local-quote/index.ts
@@ -1,11 +1,9 @@
-import { TaxCalculator } from "../tax-calculator/TaxCalculator";
 import QuoteCalculator from "./QuoteCalculator";
 import { Validation } from "./validation";
 
 const factoryQuoteCalulator = () => {
   const validation = new Validation();
-  const taxCalculator = new TaxCalculator();
-  return new QuoteCalculator(validation, taxCalculator);
+  return new QuoteCalculator(validation);
 }
 
 export default factoryQuoteCalulator()
