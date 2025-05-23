aaac6c72c7ff29a1852a376955db62a1ce499654
Samuel Martins <sfelix_martins@hotmail.com>
2025-05-23 19:32:06 -0300

    fix nucrypto iof

--- src/tax-calculator/TaxCalculator.ts
+++ src/tax-calculator/TaxCalculator.ts
@@ -5,18 +5,21 @@ interface CalculateParams {
   currencyAmount: number,
   direction: Direction,
   purpose: Purposes | null,
+  isCryptoNubank?: boolean
 }
 
 export default class TaxCalculator {
-  public static getTaxAmount({currencyAmount, direction, purpose}: CalculateParams): number {
-    const IOFRate = this.getTaxRate(purpose, direction)
+  public static getTaxAmount({currencyAmount, direction, purpose, isCryptoNubank}: CalculateParams): number {
+    const IOFRate = this.getTaxRate(purpose, direction, isCryptoNubank)
     return currencyAmount * (IOFRate/100)
   }
 
-  public static getTaxRate(purpose: Purposes | null, direction: Direction): number {
+  public static getTaxRate(purpose: Purposes | null, direction: Direction, isCryptoNubank: boolean = false): number {
     let purposeTax;
 
-    if(purpose) {
+    if(isCryptoNubank) {
+      purposeTax = settings.taxes.CRYPTO_NUBANK;
+    } else if(purpose) {
       purposeTax = settings.taxes[purpose];
     }
 
